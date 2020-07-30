3663ebeaceb27278c72cf679872de62d9dab3a65
Nova-Strife <68599948+Nova-Strife@users.noreply.github.com>
2020-07-30 21:32:17 +0600

    Revert "Revert "- FTP + Streams""
    This reverts commit e1068050a6ee2f78ac69772faae6fff85ed98349.

---
+++ MyFTP/Program.cs
@@ -0,0 +1,150 @@
+﻿using FluentFTP;
+using Renci.SshNet;
+using System;
+using System.IO;
+using System.Net;
+
+namespace MyFTP
+{
+    class Program
+    {
+        private static void WebClientUpload()
+        {
+            using (var client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential("MBaibatyrov", "4R5t6y7u_");
+                client.UploadFile("ftp://192.168.31.30/TCP.pptx.gz", WebRequestMethods.Ftp.UploadFile, @"C:\DISTR\NetCat\TCP.pptx.gz");
+            }
+        }
+
+        private static void WebClientDownload()
+        {
+            using (var client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential("MBaibatyrov", "4R5t6y7u_");
+                client.DownloadFile("ftp://192.168.31.30/TCP.pptx.gz", "TCP123.pptx.gz");
+            }
+        }
+
+        private static void FtpWebRequestDownload()
+        {
+            FtpWebRequest ftp = (FtpWebRequest)WebRequest.Create("ftp://192.168.31.30/TCP.pptx.gz");
+            ftp.Credentials = new NetworkCredential("MBaibatyrov", "4R5t6y7u_");
+            ftp.Method = WebRequestMethods.Ftp.DownloadFile;
+            using (Stream ftpStream = ftp.GetResponse().GetResponseStream())
+            using (Stream fileStream = File.Create("TCP.pptx.gz"))
+            {
+                ftpStream.CopyTo(fileStream);
+            }
+        }
+        private static void FtpWebRequestUpload()
+        {
+            FtpWebRequest ftp = (FtpWebRequest)WebRequest.Create("ftp://192.168.31.30/TCP.pptx.gz");
+            ftp.Credentials = new NetworkCredential("MBaibatyrov", "4R5t6y7u_");
+            ftp.Method = WebRequestMethods.Ftp.UploadFile;
+            var bytes = File.ReadAllBytes("TCP.pptx.gz");
+            ftp.ContentLength = bytes.Length;
+            using (Stream stream = ftp.GetRequestStream())
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        private static void FtpClientDownload()
+        {
+            FtpClient client = new FtpClient();
+            client.Host = "192.168.31.30";
+            client.Credentials = new NetworkCredential("MBaibatyrov", "4R5t6y7u_");
+            client.Connect();
+            FtpStatus Status = client.DownloadFile("TCP.pptx.gz", "/sdf/fTCP.pptx.gz");
+        }
+
+        private static void FtpClientUpload()
+        {
+            FtpClient client = new FtpClient();
+            client.Host = "192.168.31.30";
+            client.Credentials = new NetworkCredential("MBaibatyrov", "4R5t6y7u_");
+            client.Connect();
+            var Status = client.UploadFile("TCP.pptx.gz", "/TCP.pptx.gz");
+        }
+
+
+        private static void FtpClientBoth()
+        {
+            FtpClient client = new FtpClient();
+            client.Host = "ftp://ftp.dlptest.com/";
+            client.Credentials = new NetworkCredential("dlpuser@dlptest.com", "eUj8GeW55SvYaswqUyDSm5v6N");
+            client.Connect();
+            var Status = client.UploadFile("TCP.pptx.gz", "/TCP.pptx.gz");
+            Console.WriteLine($"UploadFile: {Status.ToString()}");
+            client.Rename("/TCP.pptx.gz", "/TCP666666.pptx.gz");
+
+            Status = client.DownloadFile("TCP666666.pptx.gz", "/TCP666666.pptx.gz");
+            Console.WriteLine($"DownloadFile: {Status.ToString()}");
+
+
+            ////client.DeleteFile("/TCP666666.pptx.gz");
+        }
+
+        private static void FtpClientDelete()
+        {
+            FtpClient client = new FtpClient();
+            client.Host = "ftp://ftp.dlptest.com/";
+            client.Credentials = new NetworkCredential("dlpuser@dlptest.com", "eUj8GeW55SvYaswqUyDSm5v6N");
+            client.Connect();
+            client.DeleteFile("/TCP666666.pptx.gz");
+        }
+
+
+        static void Main(string[] args)
+        {
+            //WebClientUpload();
+            //WebClientDownload();
+            //FtpWebRequestDownload();
+            //FtpWebRequestUpload();
+            //FtpClientDownload();
+            //FtpClientUpload();
+            //FtpClientBoth();
+            //FtpClientDelete();
+            SFTP();
+
+        }
+
+
+        private static void SFTP()
+        {
+            String Host = "test.rebex.net";
+            int Port = 22;
+            String RemoteFileName = "FluentFTP.dll";
+            String LocalDestinationFilename = "readme666.txt";
+            String Username = "demo";
+            String Password = "password";
+
+            using (var sftp = new SftpClient(Host, Port, Username, Password))
+            {
+                sftp.Connect();
+
+                //var files = sftp.ListDirectory("//");
+
+                //foreach (var file in files)
+                //{
+                //    Console.WriteLine(file.Name);
+                //}
+
+                //using (var file = File.OpenRead(LocalDestinationFilename))
+                //{
+                //    sftp.UploadFile(file, RemoteFileName);
+                //}
+
+
+
+                using (var file = File.OpenWrite(LocalDestinationFilename))
+                {
+                    sftp.DownloadFile("readme.txt", file);
+                }
+
+                sftp.Disconnect();
+            }
+        }
+    }
+}
---
+++ Streams/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams
+{
+    class Program
+    {
+        private static byte[] getContent_1(string fileName)
+        {
+            byte[] Content = null;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                Content = new byte[fs.Length];
+                fs.Read(Content, 0, (int)fs.Length);
+            }
+            return Content;
+        }
+        private static byte[] getContent_2(string fileName)
+        {
+            return File.ReadAllBytes(fileName);
+        }
+        private static byte[] getContent_3(string fileName)
+        {
+            byte[] Content = null;
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    Content = br.ReadBytes((int)fs.Length);
+                }
+            }
+            return Content;
+        }
+
+        private static byte[] getContent_4(string fileName)
+        {
+            byte[] Content = null;
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    fs.CopyTo(ms);
+                    Content = ms.ToArray();
+                }
+
+            }
+            return Content;
+        }
+
+        private static void Binary()
+        {
+            string FileName = @"C:\Temp\Data.xlsx";
+            var bytesContent = getContent_3(FileName);
+            var bytesFileName = new byte[1000];
+            var bytesTotal = new byte[bytesFileName.Length + bytesContent.Length];
+
+            using (BinaryWriter bw = new BinaryWriter(new MemoryStream(bytesTotal)))
+            {
+                bw.Write(Encoding.UTF8.GetBytes(Path.GetFileName(FileName)));
+                bw.Seek(999, SeekOrigin.Begin);
+                bw.Write(bytesContent);
+            }
+
+            using (BinaryReader br = new BinaryReader(new MemoryStream(bytesTotal)))
+            {
+                var bytesTemp = br.ReadBytes(999);
+                string FileNameTemp = Encoding.UTF8.GetString(bytesTemp).Replace("\0", string.Empty);
+                bytesTemp = br.ReadBytes(bytesTotal.Length - 999 - 1);
+                File.WriteAllBytes(@"C:\Temp\todo\" + FileNameTemp, bytesTemp);
+            }
+        }
+
+        private static void Memory()
+        {
+            string FileName = @"C:\Temp\Data.xlsx";
+            var bytesContent = getContent_3(FileName);
+            var bytesFileName = new byte[1000];
+            var bytesTotal = new byte[bytesFileName.Length + bytesContent.Length];
+
+            byte[] total = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var bFIleName = Encoding.UTF8.GetBytes(Path.GetFileName(FileName));
+                ms.Write(bFIleName, 0, bFIleName.Length);
+                var b = ms.ToArray();
+                ms.Position = 1000;
+                ms.Write(bytesContent, 0, bytesContent.Length);
+                total = ms.ToArray();
+            }
+
+            using (MemoryStream ms = new MemoryStream(total))
+            {
+                var bFIleName = new byte[1000];
+                ms.Read(bFIleName, 0, 999);
+                string FileNameTemp = Encoding.UTF8.GetString(bFIleName).Replace("\0", string.Empty);
+                ms.Position = 1000;
+                byte[] bContent = new byte[total.Length - 1000];
+                ms.Read(bContent, 0, (int)ms.Length - 1000);
+                File.WriteAllBytes(@"C:\Temp\todo\" + FileNameTemp, bContent);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Memory();
+        }
+    }
+}
